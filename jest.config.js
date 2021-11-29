83e9f1195c399755bfbdc1b028f40fa310aed165
xuzhouchen <xuzhouchen@bilibili.com>
2021-11-29 14:48:36 +0800

    init

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts', 'jsx', 'tsx'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  collectCoverage: process.env.COVERAGE === 'true',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!<rootDir>/src/__tests__/**/*',
+    '!<rootDir>/dist/**/*',
+  ],
+  testMatch: ['<rootDir>/src/__tests__/**/*.test.[jt]s?(x)'],
+  globals: {
+    'ts-jest': {
+      babelConfig: './babel.config.js',
+    },
+  },
+  testEnvironment: 'jest-environment-jsdom-global',
+};
